a2d364653bf01fddbfce03b242d32d660e140693
Mika <miagzgx@gmail.com>
2026-09-11 17:57:53 +0300

    Delete Models directory

--- Models/EmployeesModel/EmployeeTasksPageViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using WebBuild.Models.Enities;
-
-namespace WebBuild.Models.EmployeesModel;
-
-public class EmployeeTasksPageViewModel
-{
-    public List<EmployeeTaskListViewModel> Tasks { get; set; } = new();
-    public List<WorkTaskStatus> AvailableStatuses { get; set; } = new(); 
-}
